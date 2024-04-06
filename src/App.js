b60bd2327baf0e2895b332459a45f5d8a964556a
Phushyamithra <gauriphushyamithra@gmail.com>
2024-04-07 00:03:04 +0530

    final commit of 6-4-24

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import './styles/footer.css'
 import './styles/header.css'
 import './styles/Card.css'
@@ -9,15 +9,20 @@ import Card from './components/Card.js'
 function App() {
   const cardLoop = [];
   for(var i=0;i<10;i++){
-    cardLoop.push(<Card/>);
+    cardLoop.push(<Card />);
   }
 
   return (
     <div className="App">
         <Header/>
         <img src="sample.jpg" className="samplelogo" alt="sample image" />
-        {cardLoop}
-       <Card />
+        <div className="card-Container">
+        <div className="product-list--wrapper">
+        {[...Array(10)].map((_, index) => (
+        <Card key={index} />
+      ))}
+        </div>
+        </div>
       <Footer />
       
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,67 +1,69 @@
 import { RiInformationFill } from "react-icons/ri";
 import { FaCartPlus } from "react-icons/fa";
 
-const Card = () => {
-   
+const Card = ({ index }) => {
+
     return (
+            <div className="card">
         <div class="wrapper">
-            <div class="container">
-                <div class="top"></div>
-                <div class="bottom">
-                    <div class="left">
-                        <div class="details">
-                            <h1>Chair</h1>
-                            <p>£250</p>
+                <div class="container">
+                    <div class="top"></div>
+                    <div class="bottom">
+                        <div class="left">
+                            <div class="details">
+                                <h1>Chair</h1>
+                                <p>£250</p>
+                            </div>
+                            <div class="buy"><FaCartPlus size={30} /></div>
                         </div>
-                        <div class="buy"><FaCartPlus size={30} /></div>
-                    </div>
-                    <div class="right">
-                        <div class="done"><i class="material-icons">done</i></div>
-                        <div class="details">
-                            <h1>Chair</h1>
-                            <p>Added to your cart</p>
+                        <div class="right">
+                            <div class="done"><i class="material-icons">done</i></div>
+                            <div class="details">
+                                <h1>Chair</h1>
+                                <p>Added to your cart</p>
+                            </div>
+                            <div class="remove"><i class="material-icons">clear</i></div>
                         </div>
-                        <div class="remove"><i class="material-icons">clear</i></div>
                     </div>
                 </div>
-            </div>
-            <div class="inside">
-                <div class="icon"><RiInformationFill size={25} /></div>
-                <div class="contents">
-                    <table>
-                        <tr>
-                            <th>Width</th>
-                            <th>Height</th>
-                        </tr>
-                        <tr>
-                            <td>3000mm</td>
-                            <td>4000mm</td>
-                        </tr>
-                        <tr>
-                            <th>Something</th>
-                            <th>Something</th>
-                        </tr>
-                        <tr>
-                            <td>200mm</td>
-                            <td>200mm</td>
-                        </tr>
-                        <tr>
-                            <th>Something</th>
-                            <th>Something</th>
-                        </tr>
-                        <tr>
-                            <td>200mm</td>
-                            <td>200mm</td>
-                        </tr>
-                        <tr>
-                            <th>Something</th>
-                            <th>Something</th>
-                        </tr>
-                        <tr>
-                            <td>200mm</td>
-                            <td>200mm</td>
-                        </tr>
-                    </table>
+                <div class="inside">
+                    <div class="icon"><RiInformationFill size={25} /></div>
+                    <div class="contents">
+                        <table>
+                            <tr>
+                                <th>Width</th>
+                                <th>Height</th>
+                            </tr>
+                            <tr>
+                                <td>3000mm</td>
+                                <td>4000mm</td>
+                            </tr>
+                            <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                            </tr>
+                            <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                            </tr>
+                            <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                            </tr>
+                            <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                            </tr>
+                            <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                            </tr>
+                            <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
