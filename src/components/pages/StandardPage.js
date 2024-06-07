caf25d8eda2e6779cfca06ac4d4076041a8da422
Phushyamithra <gauriphushyamithra@gmail.com>
2024-06-08 01:31:40 +0530

    Our Story but scroll bar issue

---
+++ src/components/pages/StandardPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from '../sections/Header.js';
+import NewFooter from '../sections/NewFooter.js'
+import BackgroundMedia from './BackgroundMedia.js';
+import'../../styles/StandardPage.css';
+
+const StandardPage = ({src,type,children}) => {
+  return (
+        <>
+          <div className="main-container">
+            <Header></Header>
+              <BackgroundMedia src={src} type={type}>
+                {/* <div className="content"> */}
+                  {children}
+                {/* </div> */}
+              </BackgroundMedia>
+            <NewFooter />
+          </div>
+        </>
+  );
+}
+
+export default StandardPage
